a025e217d35fa264eb23a120e5e3f04ae371060e
faustbrian <faustbrian@users.noreply.github.com>
2023-04-26 03:39:10 +0000

    chore: resolve style guide violations

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -13,7 +13,7 @@ final class ServiceProvider extends AbstractServiceProvider
     public function packageRegistered(): void
     {
         IconFamilyRegistry::push(
-            IconFamily::fromDirectory('phosphor', __DIR__ . '/../resources/svg'),
+            IconFamily::fromDirectory('phosphor', __DIR__.'/../resources/svg'),
         );
     }
 }
